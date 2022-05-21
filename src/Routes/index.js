ff71288c535db6cb8c5d58ba2397e1922086860e
Develo It Solution <khawarqureshi420@yahoo.com>
2022-05-21 14:16:10 +0500

    push

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,13 +1,17 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Login from '../screens/Login';
 import Dashboard from '../screens/Dashboard';
 import {useSelector} from 'react-redux';
 import Loader from '../components/Loader'
+import SplashScreen from 'react-native-splash-screen'
 const Stack = createStackNavigator();
 export default function Routes() {
     const isLogin = useSelector(state => state.Auth.isLogin)
+    useEffect(()=>{
+      SplashScreen.hide();
+    },[])
   return (
     <NavigationContainer>
       <Loader/>
--- src/utills/AppColors.js
+++ src/utills/AppColors.js
@@ -1,4 +1,10 @@
 const AppColors = {
+  appBaseColor:'#fafafa',
+  btnBackgroundColorDark:'#4b4b4b',
+  btnBackgroundColorLight:'#989898',
+    placeHolderColor:'#545454',
+  appTextColor:"#929293",
+
   green: '#669e01',
   white: '#ffffff',
   transparent: 'transparent',
