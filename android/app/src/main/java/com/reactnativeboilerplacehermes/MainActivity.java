ad7c5a7e620c1af4aea0b2bcbb6fd70430bb98e3
Develo It Solution <khawarqureshi420@yahoo.com>
2022-07-29 16:51:27 +0500

    push

--- android/app/src/main/java/com/reactnativeboilerplacehermes/MainActivity.java
+++ android/app/src/main/java/com/reactnativeboilerplacehermes/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativeboilerplacehermes;
+package com.dolcedaysapp;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
